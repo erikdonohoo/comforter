38a676a838526c5993df19a49636bc2f794992cc
Erik Donohoo <erikdonohoo@gmail.com>
2016-01-12 16:19:20 -0700

    successfully obtain zip files for coverage

--- api/apps.js
+++ api/apps.js
@@ -4,15 +4,31 @@ var express = require('express');
 var router = express.Router();
 var App = require('../models/app');
 var multer = require('multer');
-var upload = multer({dest: './uploads'});
+var crypto = require('crypto');
+var path = require('path');
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './uploads/');
+	},
+	filename: function (req, file, cb) {
+		crypto.pseudoRandomBytes(16, function (err, raw) {
+			cb(null, raw.toString('hex') + '-' + Date.now() + '.' + path.extname(file.originalname));
+		});
+	}
+});
+var upload = multer({
+	storage: storage
+});
+
 var parse = require('lcov-parse');
 var math = require('mathjs');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    App.find({}).then(function (apps) {
-        res.json(apps);
-    });
+	App.find({}).then(function (apps) {
+		res.json(apps);
+	});
 });
 
 // 1. Accept coverage % or LCOV along with optional zip
@@ -27,47 +43,58 @@ router.get('/', function (req, res) {
 // 8. Report discrepancy on commit status fail with target_url to the zip of coverage (if included)
 // 9. Otherwise pass and update commit status with success and target_url to zip (if included)
 router.post('/:id/coverage', function (req, res) {
-    upload.fields([{name: 'lcov'}, {name: 'zip'}])(req, res, function (err) {
+	upload.fields([{name: 'lcov'}, {name: 'zip'}])(req, res, function (err) {
 
-        if (err) {
-            console.error(err);
-            return res.json(500, {error: err});
-        }
+		if (err) {
+			console.error(err);
+			return res.json(500, {
+				error: err
+			});
+		}
 
-        if (req.files && req.files.lcov) {
-            var lcov = req.files.lcov[0];
-            parse(lcov.path, function (err, data) {
-                res.json({coverage: coverageFromLcov(data)});
-            });
-        } else if (req.body.coverage) {
-            res.json({coverage: req.body.coverage});
-        } else {
-            res.json(400, {error: 'missing lcov or coverage information'});
-        }
-    });
+		console.log(req.files);
+
+		if (req.files && req.files.lcov) {
+			var lcov = req.files.lcov[0];
+			parse(lcov.path, function (err, data) {
+				res.json({
+					coverage: coverageFromLcov(data)
+				});
+			});
+		} else if (req.body.coverage) {
+			res.json({
+				coverage: req.body.coverage
+			});
+		} else {
+			res.json(400, {
+				error: 'missing lcov or coverage information'
+			});
+		}
+	});
 });
 
 router.post('/', function (req, res) {
-    var newApp = new App(req.body);
-    newApp.save(function (err) {
-        if (err) throw err;
-        res.json(req.body);
-    });
+	var newApp = new App(req.body);
+	newApp.save(function (err) {
+		if (err) throw err;
+		res.json(req.body);
+	});
 });
 
-function coverageFromLcov (data) {
-    var totalFound = 0, totalCovered = 0;
-    data.forEach(function (file) {
-        for (var i in file) {
-            var prop = file[i];
-            if (!!prop && (prop.constructor === Object) && prop.hit) {
-                totalFound += prop.found;
-                totalCovered += prop.hit;
-            }
-        }
-    });
+function coverageFromLcov(data) {
+	var totalFound = 0,
+		totalCovered = 0;
+	data.forEach(function (file) {
+		for (var i in file) {
+			var prop = file[i];
+			if (!!prop && (prop.constructor === Object) && prop.hit) {
+				totalFound += prop.found;
+				totalCovered += prop.hit;
+			}
+		}
+	});
 
-    return math.round((totalCovered / totalFound) * 100, 4);
+	return math.round((totalCovered / totalFound) * 100, 4);
 }
 
 module.exports = router;

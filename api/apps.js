628356195f173229c3b96e2bbfb8c8b519456197
Erik Donohoo <erikdonohoo@gmail.com>
2016-01-15 15:11:09 -0700

    add code for removing zip after unpacking and placing where it goes

--- api/apps.js
+++ api/apps.js
@@ -9,6 +9,9 @@ var path = require('path');
 var q = require('q');
 var build = require('../lib/build.js');
 var gitlabAuth = require('../lib/gitlab').gitlabAuth;
+var fs = require('fs');
+var Minizip = require('node-minizip');
+var rimraf = require('rimraf');
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -83,10 +86,33 @@ router.post('/:id/coverage', function (req, res) {
 				commit: req.body.commit,
 				branch: req.body.branch,
 				coverage: coverage,
-				hasDetails: req.files.zip != null
+				hasDetails: req.files.zip != null && req.files.zip.length
 			});
 
-			// move zip (if here) to location after unzipping
+			// delete lcov and zip
+			if (req.files.zip && req.files.zip.length) {
+				// move zip (if here) to location after unzipping and then remove
+				var target = __dirname + '../app-coverage-data/coverage/apps/' + req.body.project + '/' + req.body.branch;
+
+				var unzipAndRemove = function () {
+					Minizip.unizp(req.files.zip[0].path, target, function (err) {
+						console.error(err);
+						fs.unlink(req.files.zip[0].path);
+					});
+				};
+
+				try {
+					fs.accessSync(target, fs.F_OK);
+					rimraf(target, function () {
+						unzipAndRemove();
+					});
+				} catch (e) {
+					unzipAndRemove();
+				}
+
+			}
+
+			fs.unlink(req.files.lcov[0].path);
 
 		}).catch(function (err) {
 			console.error(err);

30a7a469fa97f02a5253d5213eda95d4154555b4
Erik Donohoo <erikdonohoo@gmail.com>
2020-10-09 15:12:26 -0600

    Add initial laravel project

---
+++ docker/web/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f /var/run/httpd/httpd.pid
+
+cd /www/api
+
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+/usr/bin/php /usr/local/bin/composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader
+
+exec /usr/sbin/httpd -D FOREGROUND

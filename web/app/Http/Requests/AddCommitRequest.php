c0fe6f60ef1665c1064ade8f548e14a5172b328c
Erik Donohoo <erikdonohoo@gmail.com>
2021-01-19 15:19:05 -0700

    Add request object for sending coverage to comforter

---
+++ web/app/Http/Requests/AddCommitRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\UploadedFile;
+
+/**
+ * AddCommitRequest class
+ *
+ * @property string $branch
+ * @property string $sha
+ * @property int $project_id
+ * @property string $coverage
+ * @property UploadedFile $zip
+ * @property UploadedFile $lcov
+ */
+class AddCommitRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'branch' => ['required', 'string'],
+            'sha' => ['required', 'string'],
+            'project_id' => ['required', 'integer'],
+            'zip' => ['required', 'file'],
+            'coverage' => ['required_without:lcov', 'numeric'],
+            'lcov' => ['required_without:coverage', 'file']
+        ];
+    }
+}
